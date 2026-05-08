d6805ce46f5a88544f08426a442ca583b936a628
Diego Paricio <32286724+diegoparicio@users.noreply.github.com>
2026-05-08 19:23:40 +0200

    Delete supermercado directory

--- supermercado/pedidos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Tickets
-
-@admin.register(Tickets)
-class PedidoAdmin(admin.ModelAdmin):
-    list_display = ('id_pedido', 'id_cliente', 'fecha')  # Campos que se mostrarán en la lista
